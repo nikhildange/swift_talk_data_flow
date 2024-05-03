31026c7349348a3f5212b42ed0414823cbd6d135
Nikhil Dange <nikhil@tickledmedia.com>
2024-05-03 14:36:13 +0530

    Initial Commit

---
+++ SwiftTalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTalk
+//
+//  Created by Nikhil on 03/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTalk/SwiftTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTalkApp.swift
+//  SwiftTalk
+//
+//  Created by Nikhil on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
